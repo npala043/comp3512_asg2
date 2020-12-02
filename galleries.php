db293260a39de73e9e694a45c6bbda43411e2ff0
mlee950 <mlee950@mtroyal.ca>
2020-12-01 19:33:27 -0700

    Created the base html for the galleries page

--- galleries.php
+++ galleries.php
@@ -9,6 +9,69 @@
 </head>
 
 <body>
-    
+    <!-- Title for galleries page -->
+    <header>
+        <h1> Galleries Page </h1>
+    </header>
+<!-- We located the html information from lab10-text05 -->
+<main>
+        <!-- Button to toggle list -->
+        <button type="button" id="toggle">Hide Gallery List</button>
+        <!-- Creates the gallery list  -->
+        <div class = "box list"> 
+            <section>
+                <h2>Galleries</h2>
+                <img src="img/loader.gif" id="loader1">
+                <ul id="galleryList">
+                    <!-- insert il using js -->
+                </ul>
+            </section>    
+        </div>
+
+        <!-- Creates the gallery info -->
+        <div class = "box info"> 
+            <section>
+                <label> </label>
+                <h2 id="galleryName"></h2>
+                <label>Native Name:</label>
+                <span id="galleryNative"></span>          
+                <label>Address:</label>
+                <span id="galleryAddress"></span>
+                <label>City:</label>
+                <span id="galleryCity"></span>          
+                <label>Country:</label>
+                <span id="galleryCountry"></span>            
+                <label>Website:</label>
+                <span><a href="" id="galleryWebsite"></a></span>
+            </section>
+        </div>
+
+        <!-- Creates the map where the gallery is located -->
+        <div class = "box map"> 
+            <p>map</p>
+            <!-- insert map given -->
+        </div>
+
+        <!-- Creates a table list of the paintings within the gallery -->
+        <div class = "box paintings"> 
+            <section>
+                <h2>Paintings</h2>
+                <img src="img/loader.gif" id="loader2">
+                <table id="paintingTable">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th id="artist">Artist</th>
+                            <th id="title">Title</th>
+                            <th id="year">Year</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </section>
+        </div>
+
+    </main>
 </body>
 </html>
