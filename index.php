8faab44e1a7c625f6f2ce371cca5914130e6a111
Darren Lam <dlam071@mtroyal.ca>
2020-12-01 18:52:46 -0700

    Changed Home Page to index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <meta charset="utf-8"/>
+   <title>index</title>
+   <link rel="stylesheet" href="css/index.css"> 
+</head>
+
+<body>
+    <header>
+        <h1>Title</h1>
+    </header>
+    <main>
+        <section id="info">
+            <h2>User Info</h2>
+        </section>
+        <section id="favorite">
+            <h2>Favorite Paintings</h2>
+        </section> 
+        <section id="like">
+            <h2>Paintings You May Like</h2>
+        </section>
+        <section id="search">
+            <input type="search" name="s" placeholder="Search Box For Painting">
+        </section>
+    </main>
+<!-- test -->
+</body>
