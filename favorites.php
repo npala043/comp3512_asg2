2116c60df68d4c2b550dfb6471b0e0c0330b9be9
npala043 <npala043@mtroyal.ca>
2020-12-01 19:54:07 -0700

    started favorites and add-to-favorites

--- favorites.php
+++ favorites.php
@@ -1,1 +1,32 @@
 <!-- Nahuel will work on this :) -->
+
+<?php
+require_once "config.inc.php";
+require_once "asg2-db-classes.inc.php";
+
+session_start();
+if (!isset($_SESSION["favourites"])) {
+    $fav = [];
+} else {
+    $fav = $_SESSION["favourites"];
+}
+?>
+
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>Favorites</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div>
+        <table>
+
+        </table>
+    </div>
+</body>
+
+</html>
