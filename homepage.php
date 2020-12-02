0032c2e958803a686922535acca5897d98dda71c
Darren Lam <dlam071@mtroyal.ca>
2020-12-01 20:24:21 -0700

    Updated homepage.php php code as well as renamed index.css to homepage.css

--- homepage.php
+++ homepage.php
@@ -1,28 +1,21 @@
-<!DOCTYPE html>
-<html>
-<head>
-   <meta charset="utf-8"/>
-   <title>index</title>
-   <link rel="stylesheet" href="css/index.css"> 
-</head>
+<?php
 
-<body>
-    <header>
-        <h1>Title</h1>
-    </header>
-    <main>
-        <section id="info">
-            <h2>User Info</h2>
-        </section>
-        <section id="favorite">
-            <h2>Favorite Paintings</h2>
-        </section> 
-        <section id="like">
-            <h2>Paintings You May Like</h2>
-        </section>
-        <section id="search">
-            <input type="search" name="s" placeholder="Search Box For Painting">
-        </section>
-    </main>
-<!-- test -->
-</body>
+if (isset($_SESSION['user']) && isset($_SESSION['pass'])) {
+    $msg = "<h2>Welcome!</h2>";
+} else {
+    $msg = "<h2>Please Login</h2>";
+}
+
+?>
+
+<html lang="en">
+    <head>
+        <title>Assignment 2</title>
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="css/homepage.css">
+    </head>
+    
+    <body>
+        <?= $msg; ?>
+    </body>
+</html>
