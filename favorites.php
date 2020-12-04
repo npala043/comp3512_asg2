0eaa912f413a119ef89828b0d5e824e7c65f27a9
npala043 <npala043@mtroyal.ca>
2020-12-03 17:25:06 -0700

    continuing work, incomplete

--- favorites.php
+++ favorites.php
@@ -2,15 +2,26 @@
 
 <?php
 session_start();
-if (!isset($_SESSION["favourites"])) { // manually added paintings for testing
-    $fav[] = 5;
-    $fav[] = 7;
-    $fav[] = 8;
-} else {
-    $fav = $_SESSION["favourites"];
+// if (!isset($_SESSION["favourites"])) { // manually added paintings for testing
+//     $fav = [];
+// } else {
+//     $fav = $_SESSION["favourites"];
+// }
+
+// For testing
+$fav = [];
+$fav[] = 5;
+$fav[] = 7;
+$fav[] = 8;
+
+$paintings = file_get_contents("http://localhost/web2/f2020-assign2-master/api-paintings.php");
+$allPaintings = json_decode($paintings, true);
+
+foreach ($allPaintings as $p) {
+    if ($p['PaintingID'] == 0) {
+    }
 }
-
-
+// For testing
 
 ?>
 
@@ -33,14 +44,18 @@ if (!isset($_SESSION["favourites"])) { // manually added paintings for testing
                 <th>Year</th>
             </tr>
             <?php
-            foreach ($fav as $f) { ?>
-                <tr>
-                    <td></td> <!-- Painting Thumbnail -->
-                    <td></td> <!-- Artist -->
-                    <td></td> <!-- Title -->
-                    <td></td> <!-- Year -->
-                </tr>
-            <?php } ?>
+            if (empty($fav)) {
+                echo "No favourites to display!";
+            } else {
+                foreach ($fav as $f) { ?>
+                    <tr>
+                        <td></td> <!-- Painting Thumbnail -->
+                        <td><?= $f ?></td> <!-- Artist -->
+                        <td></td> <!-- Title -->
+                        <td></td> <!-- Year -->
+                    </tr>
+            <?php }
+            } ?>
         </table>
     </div>
 </body>
