6ef616ea9532af11e5d12470766a84230c51a393
halna399 <halna399@mtroyal.ca>
2020-12-05 14:25:41 -0700

    added the nav bar

--- header.php
+++ header.php
@@ -1,4 +1,18 @@
+
+
+
 <header>
     <h2>Assignment 2</h2>
-    <p>Hatoon Alnakshabandi, Anthony Dang, Darren Lam, Mason Lee, Nahuel Paladino</p>
+    <h5>Hatoon Al-Nakshabandi, Anthony Dang, Darren Lam, Mason Lee, Nahuel Paladino</h5>
 </header>
+
+<nav>
+    <a href="homepage.php">Home</a>
+    <a href="about.php">About</a>
+    <a href="browse-paintings.php">Browse Paintings</a>
+    <a href="favorites.php">Favorties</a>
+    <a href="login.php">Login</a>
+    
+
+
+</nav>
