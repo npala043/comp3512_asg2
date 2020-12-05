f6a45c527861d363a75824be34ac46b7a2844152
npala043 <npala043@mtroyal.ca>
2020-12-05 15:59:33 -0700

    got basic functionality down

--- login.php
+++ login.php
@@ -10,17 +10,23 @@ $msg = "";
 if (loginDataPresent()) {
     try {
         $connection = DatabaseHelper::createConnection(array(DBCONNSTRING, DBUSER, DBPASS));
-        $gate = new CustomerLoginDB($connection);
+        $gate = new CustomerLogonDB($connection);
         $data = $gate->getByUserName($_POST['email']);
-        $connection = null;
 
-        if (isset($data['pass'])) { // if matching user was found, does pass match?
-            if (password_verify($_POST['pass'], $data['pass'])) { // uses bcrypt
 
+        if (isset($data['Pass'])) {
+            // if matching user was found, does pass match?
+            if (password_verify($_POST['pass'], $data['Pass'])) { // uses bcrypt
+                $_SESSION['user'] = $data['CustomerID'];
+                header("Location: homepage.php");
+                exit();
+            } else {
+                $msg = "Incorrect password";
             }
         } else {
             $msg = "User not found";
         }
+        $connection = null;
     } catch (Exception $e) {
         die($e->getMessage());
     }
@@ -46,6 +52,7 @@ function loginDataPresent()
     <div>
         <h2>Login</h2>
         <p><?= $msg ?></p>
+        <p>Testing email: hholy@gmail.com</p>
         <form action="login.php" method="post">
             <label for="email">Email</label>
             <input type="email" name="email">
