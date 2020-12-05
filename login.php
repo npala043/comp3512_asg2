86de91084bf6f1565482398b4e1e8400223646fd
npala043 <npala043@mtroyal.ca>
2020-12-05 15:17:08 -0700

    starter code

--- login.php
+++ login.php
@@ -1,1 +1,23 @@
 <!-- Nahuel is working on this -->
+
+<?php
+
+
+
+?>
+
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>Login</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <?php include("header.php"); ?>
+    <div>
+
+    </div>
+</body>
