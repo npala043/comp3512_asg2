d0567d39335d866999d3b8f0fd320cdf3b2a144d
halna399 <halna399@mtroyal.ca>
2020-12-05 22:24:28 -0700

    added the logout button session functionality

--- header.php
+++ header.php
@@ -1,6 +1,3 @@
-
-
-
 <header>
     <h2>Assignment 2</h2>
     <h5>Hatoon Al-Nakshabandi, Anthony Dang, Darren Lam, Mason Lee, Nahuel Paladino</h5>
@@ -10,9 +7,18 @@
     <a href="homepage.php">Home</a>
     <a href="about.php">About</a>
     <a href="browse-paintings.php">Browse Paintings</a>
-    <a href="favorites.php">Favorties</a>
-    <a href="login.php">Login</a>
-    
+    <!-- should only be avaliable if the user is logged in -->
+    <?php
+    if (isset($_SESSION['user'])) {
+        echo  "<a href=" . 'favorites.php' . ">Favorties</a>";
+        echo  "<a href=" . 'logout.php' . ">Logout</a>";
+    } else {
+        echo  "<a href=" . 'login.php' . ">Login</a>";
+    }
+    ?>
+
+    <!-- <a href="login.php">Login</a> -->
+
 
 
 </nav>
