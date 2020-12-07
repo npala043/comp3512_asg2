736c3cf7f60beba765b83fce0f3c4cb45bdf4841
Darren Lam <dlam071@mtroyal.ca>
2020-12-06 17:53:00 -0700

    Recommended functionality working properly

--- homepage.php
+++ homepage.php
@@ -104,29 +104,100 @@ if (isset($_SESSION['user'])) {
             <?php
             if (isset($_SESSION['favourites'])) {
                 $favs = $_SESSION['favourites'];
+                $paintings = array();
                 $artist = $favs[0]['artistid'];
+                $year = $favs[0]['yearofwork'];
+                $minYear = 0;
+                $maxYear = 0;
+
+                foreach ($favs as $f) {
+                    $paintings[] = $f['id'];
+                }
+
+                if ($year < 1400) {
+                    $maxYear = 1399;
+                } elseif ($year >= 1400 && $year < 1550) {
+                    $minYear = 1400;
+                    $maxYear = 1549;
+                } elseif ($year >= 1550 && $year < 1700) {
+                    $minYear = 1550;
+                    $maxYear = 1699;
+                } elseif ($year >= 1700 && $year < 1875) {
+                    $minYear = 1700;
+                    $maxYear = 1874;
+                } elseif ($year >= 1875 && $year < 1945) {
+                    $minYear = 1875;
+                    $maxYear = 1944;
+                } else {
+                    $minYear = 1945;
+                    $maxYear = 2020;
+                }
 
                 try {
                     $conn = DatabaseHelper::createConnection(array(DBCONNSTRING, DBUSER, DBPASS));
-                    $paintGate = new PaintingDB($conn);
-                    $paintData = $paintGate->getAllForArtist($artist);
+                    $artistGate = new PaintingDB($conn);
+                    $artistData = $artistGate->getAllForArtist($artist);
+
+                    $yearGate = new PaintingDB($conn);
+                    $yearData = $yearGate->getAllSortByYear();
+                } catch (Exception $e) {
+                    die($e->getMessage());
+                }
+
+                $count = 0;
+                $added = 0;
+
+                for ($i = 0; $i < count($artistData) && $added < 6; $i++) {
+                    print_r($paintings);
+                    if (!in_array($artistData[$i]['PaintingID'], $paintings)) {
+                        $count++;
+                        $added++; ?>
+                        <a class="likeArtist" href="single-painting.php?id=<?= $artistData[$i]['PaintingID'] ?>">
+                            <img src="images/paintings/square-medium/<?= $artistData[$i]['ImageFileName'] ?>.jpg" alt="<?= $artistData[$i]['Title'] ?>">
+                        </a>
+                        <?php
+                        $paintings[] = $artistData[$i]['PaintingID'];
+                    }
+                }
+
+                $added = 0;
+
+                for ($i = 0; $added < 6; $i++) {
+                    if ($yearData[$i]['YearOfWork'] >= $minYear && $yearData[$i]['YearOfWork'] <= $maxYear) {
+                        if (!in_array($yearData[$i]['PaintingID'], $paintings)) {
+                            $count++;
+                            $added++; ?>
+                            <a class="likeEra" href="single-painting.php?id=<?= $yearData[$i]['PaintingID'] ?>">
+                                <img src="images/paintings/square-medium/<?= $yearData[$i]['ImageFileName'] ?>.jpg" alt="<?= $yearData[$i]['Title'] ?>">
+                            </a>
+                        <?php
+                            $paintings[] = $yearData[$i]['PaintingID'];
+                        }
+                    }
+                }
+
+                if ($count < 12) {
+                    $artistData = $artistGate->getAll();
+                    for ($i = 0; $count < 12; $i++, $count++) { ?>
+                        <a class="fill" href="single-painting.php?id=<?= $artistData[$i]['PaintingID'] ?>">
+                            <img src="images/paintings/square-medium/<?= $artistData[$i]['ImageFileName'] ?>.jpg" alt="<?= $artistData[$i]['Title'] ?>">
+                        </a>
+                    <?php }
+                }
+            } else {
+                try {
+                    $conn = DatabaseHelper::createConnection(array(DBCONNSTRING, DBUSER, DBPASS));
+                    $artistGate = new PaintingDB($conn);
+                    $artistData = $artistGate->getAll();
                 } catch (Exception $e) {
                     die($e->getMessage());
                 }
                 $count = 0;
-                for ($i = 0; $i < count($paintData) && $i < 6; $i++, $count++) { ?>
-                    <a href="single-painting.php?id=<?= $paintData[$i]['PaintingID'] ?>">
-                        <img src="images/paintings/square-medium/<?= $paintData[$i]['ImageFileName'] ?>.jpg" alt="<?= $paintData[$i]['Title'] ?>">
+                for ($i = 0; $count < 12; $i++, $count++) { ?>
+                    <a class="fill" href="single-painting.php?id=<?= $artistData[$i]['PaintingID'] ?>">
+                        <img src="images/paintings/square-medium/<?= $artistData[$i]['ImageFileName'] ?>.jpg" alt="<?= $artistData[$i]['Title'] ?>">
                     </a>
-                    <?php }
-                if ($count < 12) {
-                    $paintData = $paintGate->getAll();
-                    for ($i = 0; $count < 12; $i++, $count++) { ?>
-                        <a href="single-painting.php?id=<?= $paintData[$i]['PaintingID'] ?>">
-                            <img src="images/paintings/square-medium/<?= $paintData[$i]['ImageFileName'] ?>.jpg" alt="<?= $paintData[$i]['Title'] ?>">
-                        </a>
             <?php }
-                }
             }
             ?>
         </div>
