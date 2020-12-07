aaab39b34a4f7ba23f5eaf4571d32baf84699f38
npala043 <npala043@mtroyal.ca>
2020-12-07 12:02:09 -0700

    fixed db connection stuff

--- config.inc.php
+++ config.inc.php
@@ -13,14 +13,14 @@ ini_set('display_errors', '1');
 
 /*manually setting because of getenv() error 
 (it returns null for some reason, why can't it read?)*/
-define('DBUSER', 'q4mc92l607e1seit');
-define('DBPASS', 'wluibgz1btfzeal4');
-define('DBCONNSTRING', "mysql:host=ixnzh1cxch6rtdrx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=yg5mav5uvc4wjekq;charset=utf8mb4;");
+// define('DBUSER', 'q4mc92l607e1seit');
+// define('DBPASS', 'wluibgz1btfzeal4');
+// define('DBCONNSTRING', "mysql:host=ixnzh1cxch6rtdrx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=yg5mav5uvc4wjekq;charset=utf8mb4;");
 
 /* Old db connection info */
-// define('DBHOST', 'localhost');
-// define('DBNAME', 'art');
-// define('DBUSER', 'root');
-// define('DBPASS', '');
-// // define('DBCONNSTRING', 'sqlite:./databases/art.db'); // <-- SQLite
-// define('DBCONNSTRING', "mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;"); // <-- MySQL
+define('DBHOST', 'localhost');
+define('DBNAME', 'art');
+define('DBUSER', 'root');
+define('DBPASS', '');
+// define('DBCONNSTRING', 'sqlite:./databases/art.db'); // <-- SQLite
+define('DBCONNSTRING', "mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;"); // <-- MySQL
