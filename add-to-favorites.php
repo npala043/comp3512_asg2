6367ae31e31bd71aac050049ee5c037a639b1dd6
npala043 <npala043@mtroyal.ca>
2020-12-07 15:49:08 -0700

    refactored to not redirect anymore

--- add-to-favorites.php
+++ add-to-favorites.php
@@ -2,58 +2,39 @@
 
 <?php
 
-session_start();
+function addToFavorites($id, $artistid, $title, $filename, $yearofwork)
+{
+    if (!isset($_SESSION['user'])) {
+        echo "<script>alert('Please login to access favourites');</script>";
+    }
 
-if (!isset($_SESSION['user'])) {
-    $_SESSION['error'] = "<script>alert('Please login to access favourites');</script>";
-    header("Location:" . $_SERVER["HTTP_REFERER"]);
-    exit();
-}
+    // do we have a favourites array already?
+    if (!isset($_SESSION["favourites"])) {
+        // Doesn't exist, so init one
+        $_SESSION["favourites"] = [];
+    }
 
-// do we have a favourites array already?
-if (!isset($_SESSION["favourites"])) {
-    // Doesn't exist, so init one
-    $_SESSION["favourites"] = [];
-}
+    // retrieve any existing favourites
+    $fav = $_SESSION["favourites"];
 
-// retrieve any existing favourites
-$fav = $_SESSION["favourites"];
-
-// check if id, artistID, title, and filename are present as querystring
-if (allInfoPresent()) {
-
+    // check if id, artistID, title, and filename are present as querystring
     // check if painting already in favourites
     // if yes, save error message to session and redirect back
     foreach ($fav as $f) {
         if (in_array($_GET['id'], $f)) {
-            $_SESSION['error'] = "<script>alert('Already added to favourites');</script>";
-            header("Location:" . $_SERVER["HTTP_REFERER"]);
-            exit();
+            echo "<script>alert('Already added to favourites');</script>";
         }
     }
-
     // add painting to array
     $fav[] = array(
-        "id" => $_GET['id'],
-        "artistid" => $_GET['artistid'],
-        "title" => $_GET['title'],
-        "filename" => $_GET['filename'],
-        "yearofwork" => $_GET['yearofwork']
+        "id" => $id,
+        "artistid" => $artistid,
+        "title" => $title,
+        "filename" => $filename,
+        "yearofwork" => $yearofwork
     );
-
     // re-save modified array back to session state
     $_SESSION["favourites"] = $fav;
-} else {
-    $_SESSION['error'] = "<script>alert('Invalid query')</script>";
-}
-
-// redirect back to the requesting page
-header("Location:" . $_SERVER["HTTP_REFERER"]);
-exit();
-
-function allInfoPresent()
-{
-    return isset($_GET['id']) && isset($_GET['artistid']) && isset($_GET['title']) && isset($_GET['filename']) && isset($_GET['yearofwork']);
 }
 
 ?>
