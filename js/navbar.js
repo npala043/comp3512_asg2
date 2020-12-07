ad60784451fd046127ef60e9fb8065f9d0509a17
halna399 <halna399@mtroyal.ca>
2020-12-06 22:01:45 -0700

    got rid of the console logs

--- js/navbar.js
+++ js/navbar.js
@@ -2,12 +2,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
     let hamburger = document.querySelector(".toggle");
     let navBar = document.querySelector("nav .navList");
-    console.log("heeloo?");
-    console.log(navBar);
-    console.log(hamburger);
+
 
     hamburger.addEventListener('click', function() {
-        console.log('hello')
+        
         navBar.classList.toggle("active");
     });
 
