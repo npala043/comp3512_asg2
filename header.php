1bf26eed2ab6346701720bc35bdc8213af65951f
halna399 <halna399@mtroyal.ca>
2020-12-09 16:13:37 -0700

    added comments

--- header.php
+++ header.php
@@ -1,16 +1,15 @@
 <header class="header">
     <nav id="navbar">
         <img src="images/logo/hamburger-menu.jpg" class="toggle">
-        <!-- <img src="images/logo/logo.jpg" alt="log" id="logo"> -->
         <img src="images/logo/owenlogo.jpg" alt="log" id="logo">
         <ul class="navList">
             <li><a href="homepage.php">Home</a></li>
             <li><a href="about.php">About</a></li>
             <li><a href="galleries.php">Galleries</a></li>
             <li><a href="browse-paintings.php">Browse Paintings</a></li>
-            <!-- should only be avaliable if the user is logged in -->
             <?php
             if (isset($_SESSION['user'])) {
+                // should only be avaliable if the user is logged in 
                 echo  "<li><a href=" . 'favorites.php' . ">Favorites</a></li>";
                 echo  "<li><a href=" . 'logout.php' . ">Logout</a></li>";
             } else {
